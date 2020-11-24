6e6805541d44ae1b73ea5d7d49d258c25fb6ca2f
Tatyana Krempina <krempinatatyana@gmail.com>
2020-11-25 02:38:55 +0300

    Правки к 4-ому уроку. Исправление слитного написания оператора И вместе с цифрой

--- script.js
+++ script.js
@@ -68,9 +68,9 @@ console.log(budgetDay);
 let getStatusIncom = function(){
    if (budgetDay >= 1200) {
       console.log('У вас высокий уровень дохода');
-      } else if (budgetDay>=600&& budgetDay<1200) {
+      } else if (budgetDay>=600 && budgetDay<1200) {
       console.log('У вас средний уровень дохода');
-      } else if (budgetDay>=0&& budgetDay<600) {
+      } else if (budgetDay>=0 && budgetDay<600) {
       console.log('К сожалению у вас уровень дохода ниже среднего');
       } else {
       console.log('Что-то пошло не так');
