7afcde6a35eec0bebfb6955460fb3f68d4d534c6
Tatyana Krempina <krempinatatyana@gmail.com>
2020-11-28 11:57:44 +0300

    lesson06

--- script.js
+++ script.js
@@ -1,97 +1,50 @@
-'use strict'
+'use strict';
 
 let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n)
+    return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let income = '50000';
-let mission = 500000;
-let period = 6;
 
+function Init () {
+    let numRandom = Math.floor(Math.random() * 101);
 
-
-let money;
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-
-
-let expenses = []; 
-
-
-
-//LESSON05
-//1) Переписать функцию start циклом do while
-let start = function() {
-
-    do {
-        money = prompt('Ваш месячный доход?');
-    } while (!isNumber(money));
-};
-
-start();
-
-let showTypeOf = function(data) {
-    console.log(data, typeof(data));
-};
-
-showTypeOf(money);
-
-/*2) Добавить проверку что введённые данные являются числом, 
-которые мы получаем на вопрос 'Во сколько это обойдется?’ в функции  getExpensesMonth*/
-let getExpensesMonth = function() {
-    let sum = 0;
-    let pr;
-
-    for (let i = 0; i < 2; i++) {
-
-        expenses[i] = prompt('Введите обязательную статью расходов?');
-        
-        do {
-        pr = prompt('Во сколько это обойдется?');
-        } while (!isNumber(pr));
-
-        sum += +pr;
-    }
-        
+function chatBot (numRandom) {
     
-    console.log(sum);
-    return sum;
+    let num = prompt("Угадай число от 1 до 100");
+
+    if (num === null ) { 
+        alert("Игра окончена"); 
+        return;
+        }
+
+    function checkTheNum () {
+        
+        if (!isNumber(num)) { 
+            num = prompt("Введи число"); 
+            checkTheNum();
+            }
+    } 
+
+    checkTheNum();
+
+    if (numRandom < num) { 
+        alert("Загаданное число меньше");
+        } else if (numRandom > num) {
+        alert("Загаданное число больше"); 
+        } else if (Number(numRandom) === Number(num)) { 
+        alert("Поздравляю, Вы угадали!!!");
+        return;
+        }
+    
+    
+    if (num !== numRandom) { chatBot(numRandom); }
 };
 
-let expensesAmount = getExpensesMonth();
+chatBot(numRandom);
 
-console.log('Расходы за месяц: ' + expensesAmount);
-
-console.log( addExpenses.toLowerCase().split(',') );
-
-function getAccumulatedMonth(money) {
-    return Number(money)-expensesAmount;
 };
 
-let accumulatedMonth  = getAccumulatedMonth(money);
-
-/*3) Если getTargetMonth возвращает нам отрицательное значение, то вместо “Цель будет достигнута”
-необходимо выводить “Цель не будет достигнута”*/
-function getTargetMonth(mission, accumulatedMonth) {
-    return Math.ceil(mission/accumulatedMonth) < 0 ? 'Цель не будет достигнута' : 'Цель будет достигнута';
-};
-
-console.log( getTargetMonth(mission, accumulatedMonth) );
-
-let budgetDay = accumulatedMonth/30;
-console.log(budgetDay);
+Init();
 
 
-let getStatusIncom = function(){
-if (budgetDay >= 1200) {
-   console.log('У вас высокий уровень дохода');
-   } else if (budgetDay>=600 && budgetDay<1200) {
-   console.log('У вас средний уровень дохода');
-   } else if (budgetDay>=0 && budgetDay<600) {
-   console.log('К сожалению у вас уровень дохода ниже среднего');
-   } else {
-   console.log('Что-то пошло не так');
-   }
-};
 
-console.log( getStatusIncom() );
