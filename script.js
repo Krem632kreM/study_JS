3b876b8b8b1f48291e1534ba001b73bb2775b032
Tatyana Krempina <krempinatatyana@gmail.com>
2020-12-01 08:30:55 +0400

    lesson07_task_hard_commit_1

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-/*let week = ["Понедельник", 'Вторник', "Среда", "Четверг", "Пятница", "Суббота", "Воскресение"];
+'use strict';
 
- for (let i=0; i<week.length; i++) {
-    console.log(week[i]);
-    if (i === 6 || i === 7) ? 'strong' : 'span';
- };*/
-
- 'use strict';
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
 const todayDay = new Date();
 
 const days = () => {
     week.forEach((item, i) => {
-        /*if (i === +todayDay.getDay()-1) { // Если текущий день недели то от номера текущей даты отнимаем единицу, 
-            //так как массив у нас начинается с нуля
-            document.write(String(todayDay.getDay()).bold());
-    
-        }*/
-        if (item === 'Суббота' || item === 'Воскресенье') { // Если выходные то
+
+        if (item === 'Суббота' || item === 'Воскресенье') { 
             document.write(week[i].italics()+ "<pre><\/pre>");
             } else if (i === +todayDay.getDay()-1) {
-                document.write(week[i].bold()+ "<pre><\/pre>");
-                
+                document.write(week[i].bold()+ "<pre><\/pre>");    
             } else {
             document.write(week[i] + "<pre><\/pre>");
             }
     });
 };
-days(); // Вызываем функцию;
+
+days();
