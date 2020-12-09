a8cf5696e8131086345095599c2acb99d15a26c6
Tatyana <krempinatatyana@gmail.com>
2020-12-09 09:23:40 +0400

    Добавлен атрибут required в инпут header-input

--- script.js
+++ script.js
@@ -5,20 +5,16 @@ const headerInput = document.querySelector(".header-input");
 const todoList = document.querySelector(".todo-list");
 const todoCompleted =document.querySelector(".todo-completed");
 
-let todoData = [
-
-];
+let todoData = [];
 
 if (localStorage.getItem('todoData')) todoData = JSON.parse (localStorage.getItem ("todoData"));
-
-console.log(todoData);
 
 const render = function() {
 
     todoList.textContent = '';
     todoCompleted.textContent = '';
     
-todoData.forEach(function(item, index, arr){
+    todoData.forEach(function(item, index, arr){
     const li = document.createElement('li');
     li.classList.add('todo-item');
 
@@ -28,11 +24,11 @@ todoData.forEach(function(item, index, arr){
         '<button class="todo-complete"></button>' + 
     '</div>';
 
-if (item.comleted) {
-        todoCompleted.append(li);
-    } else {
-        todoList.append(li);
-    }
+    if (item.comleted) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
 
     const btnTodoCompleted = li.querySelector('.todo-complete');
 
@@ -45,58 +41,17 @@ if (item.comleted) {
    const btnTodoRemoved = li.querySelector('.todo-remove');
 
     btnTodoRemoved.addEventListener ('click', function(e){
-        /*const btn = e.target.closest('.todo-remove');
-        if (!btn) {
-        return;
-        }
-        btn.closest('li').remove();*/
-        //todoList.remove(index);
-           arr.splice(index,1);
-           /*delete todoData[index];
-           todoData = todoData.filter(function (el) {
-            return el !== null;
-        });*/
-    
-           localStorage.removeItem('todoData');
-           localStorage.setItem ("todoData", JSON.stringify(todoData));
-           console.log(todoData);
-           render();
+        arr.splice(index,1);  
+        localStorage.removeItem('todoData');
+        localStorage.setItem ("todoData", JSON.stringify(todoData));
+        render();
         });
         
-});
+    });
 
-
-
-
-
-
-
-    /*btnTodoRemoved.onclick = function(e) {
-        const btn = e.target.closest('.todo-remove');
-        if (!btn) {
-        return;
-        }
-        //btn.parentElement.remove();
-        btn.closest('li').remove();
-
-        todoData.splise(index,1);
-
-    
-        
-        console.log(item);
-        //localStorage.removeItem("todoData");
-        //localStorage.setItem ("todoData", JSON.stringify(todoData));
-        //if (localStorage.getItem('todoData')) todoData = JSON.parse (localStorage.getItem ("todoData"));
-    }
-
-//console.log(todoData);*/
-};
+    };
 
 render();
-
-console.log(todoData);
-
-
 
 todoControl.addEventListener('submit', function(event){
 
@@ -109,24 +64,22 @@ let newTodo = {
 todoData.push(newTodo);
 localStorage.setItem ("todoData", JSON.stringify(todoData));
 render();
-console.log("ffff" + headerInput.value);
 headerInput.value = '';
 });
-//localStorage.setItem ("todoData", JSON.stringify(todoData));
-const headerButton  = document.querySelector(".header-button");
+
+const headerButton  = document.getElementById('add');
 headerButton.addEventListener("input", buttonBlocked);
 function buttonBlocked() {
     
-    if (headerInput.value === '') { 
+    if (headerInput.value) { 
         headerButton.disabled = true; 
         } else {
             headerButton.disabled = false;
         }   
     }
-  
-  buttonBlocked();
+buttonBlocked();
 
-  
+
 
 
 
