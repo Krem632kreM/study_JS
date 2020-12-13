b6245284e40e635bfc19689b8396b450ba8a5288
Tatyana <krempinatatyana@gmail.com>
2020-12-13 16:23:45 +0400

    Исправлено название класса

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 'use strict';
 
-/*const DomElement = function() {
-    this.selector = '.gfh';
-    this.height = 0;
-    this.width = 0;
-    this.bg = 0;
-    this.fontSize = 0;
-};
-
-DomElement.prototype.createDiv = function() {
-    console.log(this.selector);
-}.bind(DomElement);
-
-DomElement.prototype.createDiv.bind(DomElement);*/
-
-function Car(selector, height, width, bg, fontSize  ){
+function DomElement(selector, height, width, bg, fontSize  ){
     this.selector = selector;
     this.height = height;
     this.width = width;
@@ -22,7 +8,7 @@ function Car(selector, height, width, bg, fontSize  ){
     this.fontSize = fontSize;
   }
   
-  Car.prototype.createDiv = function(){
+  DomElement.prototype.createDiv = function(){
     if (this.selector.startsWith('.')) {
         const div = document.createElement('div');
         div.innerHTML = '<div class="' + this.selector.split('.').pop() + '">див с классом</div>';
@@ -38,15 +24,9 @@ function Car(selector, height, width, bg, fontSize  ){
         div.style.cssText = 'height:' + this.height +'; width:' + this.width + 
         '; background:' + this.bg + '; font-size:' + this.fontSize + '; color: black;';
     }
-
   };
   
-  Car.prototype.getInformation = function(){
-    return `${this.selector} ${this.height}, ${this.width}`;
-  };
-  
-  let obj = new Car('.block,' ,  '50px' , '400px' , '#FE8020' , '40px');
+  let obj = new DomElement('.block,' ,  '50px' , '400px' , '#FE8020' , '40px');
   
   obj.createDiv();
   
-  console.log(obj.getInformation()); //Audi S8, 2020
