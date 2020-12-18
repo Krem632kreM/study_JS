78dc87923843e8e4396b2c77cb0317b71de3ff2b
Tatyana <krempinatatyana@gmail.com>
2020-12-18 21:31:49 +0400

    lesson18_2

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
+
 'use strict';
+      let MyDate = new Date(),
+      str = '',
+      MyHours = MyDate.getHours();
+      switch (true){
+          case (MyHours >= 5) && (MyHours < 11): str ='Доброе утро';
+          break;
+          case (MyHours >= 11) && (MyHours < 16): str ='Добрый день';
+          break;
+          case (MyHours >= 16) && (MyHours <= 23): str ='Добрый вечер';
+          break;
+          case (MyHours >= 0) && (MyHours < 5): str = 'Доброй ночи';
+          break;
+      }
 
-function DomElement(selector, height, width, bg, fontSize  ){
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-  }
-  
-  DomElement.prototype.createDiv = function(){
-    if (this.selector.startsWith('.')) {
-        const div = document.createElement('div');
-        div.innerHTML = '<div class="' + this.selector.split('.').pop() + '">див с классом</div>';
-        document.body.append(div);
-        div.style.cssText = 'height:' + this.height +'; width:' + this.width + 
-        '; background:' + this.bg + '; font-size:' + this.fontSize + '; color: black;';
+      function getWeekDay(date) {
+        date = date || new Date();
+        var days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+        var day = date.getDay();
+    
+        return days[day];
     }
 
-    if (this.selector.startsWith('#')) {
-        const div = document.createElement('div');
-        div.innerHTML = '<div id="' + this.selector.split('.').pop() + '">параграф</div>';
-        document.body.append(div);
-        div.style.cssText = 'height:' + this.height +'; width:' + this.width + 
-        '; background:' + this.bg + '; font-size:' + this.fontSize + '; color: black;';
-    }
-  };
-  
-  let obj = new DomElement('.block,' ,  '50px' , '400px' , '#FE8020' , '40px');
-  
-  obj.createDiv();
-  
+    
+
+let time = new Date();
+let localTime = time.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit',hour12: true })
+
+let dateNewYear = new Date('01-01-2021');
+let daysLag = Math.ceil(Math.abs(dateNewYear.getTime() - time.getTime()) / (1000 * 3600 * 24));
+
+document.body.innerHTML = str + "<br/>Сегодня " + getWeekDay(MyDate) + "<br/>Текущее время: " + localTime + "<br/> До нового года осталось " + daysLag +" дней";
